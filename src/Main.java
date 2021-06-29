ff1937dd4bcbde7b42265337eee0b5b41cb19ffd
Sofiia Cherednychenko <ruslanovna1604@gmail.com>
2021-06-29 20:38:42 +0300

    update method Recurse to recurse

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        RecurseMass(array, 0);
+        recurseMass(array, 0);
 
         OwnList list = new OwnList();
         for (int i = 0; i <= 20; i++) {
@@ -16,14 +16,14 @@ public class Main {
 
     }
 
-    public static void RecurseMass(int[] array, int start) {
+    public static void recurseMass(int[] array, int start) {
         if (start == array.length - 1) {
             System.out.print(array[start]);
             return;
         } else {
             System.out.print(array[start] + ",");
             start++;
-            RecurseMass(array, start);
+            recurseMass(array, start);
         }
     }
 
